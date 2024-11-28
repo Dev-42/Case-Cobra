f202c5ee4105adda329695fefb8a4eb186677a0b
Dev Bhattacharya <devbhattacharya42@gmail.com>
2024-11-28 22:45:46 +0530

    Made the navbar and designed it.
    Made the navbar and designed it.

--- casecraft/src/app/page.tsx
+++ casecraft/src/app/page.tsx
@@ -21,8 +21,8 @@ export default function Home() {
         <MaxWidthWrapper className="pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52 max-w-screen mx-auto">
             <div className='col-span-2 px-6 lg:px-0 lg:pt-4'>
                 <div className='relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start'>
-                  <div className='absolute w-28 left-0 top-20 hidden lg:block'>
-                      <Image src={Logo} alt='logoImage' className='w-full'></Image>
+                  <div className='absolute w-28 left-[-6px] top-[-3rem] hidden lg:block'>
+                      <Image src={Logo} alt='logoImage' className='w-full h-full rounded-full object-cover'></Image>
                   </div>
                   <h1 className='relative w-fit tracking-tight text-balance mt-16 font-bont !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl'>
                     Your Image on a <span className='bg-green-600 px-2 text-white'>Custom</span> Phone Case
