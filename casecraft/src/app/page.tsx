29055bcfe3ec1f82556450726b7c44ed023bfeb3
Dev Bhattacharya <devbhattacharya42@gmail.com>
2025-03-03 20:23:45 +0530

    Create case page in progress

--- casecraft/src/app/page.tsx
+++ casecraft/src/app/page.tsx
@@ -89,17 +89,17 @@ export default function Home() {
       <section className='bg-slate-100 py-24'>
           <MaxWidthWrapper className='flex flex-col items-center gap-16 sm:gap-32'>
               <div className='flex flex-col lg:flex-row items-center gap-4 sm:gap-6'>
-                 <h2 className='order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900'>What our <span className='relative px-2 '>customers<Icons.underline className='hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-green-500'/></span> say</h2> 
+                 <h2 className='order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900'>What our <span className='relative px-2 '>customers<Icons.underline className='hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-primary'/></span> say</h2> 
                  <Image src={SnakeImg} alt='snakeImg' className='w-24 order-0 lg:order-2'/>
               </div>
               <div className='mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16'>
                 <div className='flex flex-auto flex-col gap-4 lg:pr-8 xl-pr-20'>
                   <div className='flex gap-0.5 mb-2'>
-                    <Star className='h-5 w-5 text-green-600 fill-green-600 '/>
-                    <Star className='h-5 w-5 text-green-600 fill-green-600 '/>
-                    <Star className='h-5 w-5 text-green-600 fill-green-600 '/>
-                    <Star className='h-5 w-5 text-green-600 fill-green-600 '/>
-                    <Star className='h-5 w-5 text-green-600 fill-green-600 '/>
+                    <Star className='h-5 w-5 text-[#fdcc06] fill-[#fdcc06] '/>
+                    <Star className='h-5 w-5 text-[#fdcc06] fill-[#fdcc06] '/>
+                    <Star className='h-5 w-5 text-[#fdcc06] fill-[#fdcc06] '/>
+                    <Star className='h-5 w-5 text-[#fdcc06] fill-[#fdcc06]'/>
+                    <Star className='h-5 w-5 text-[#fdcc06] fill-[#fdcc06] '/>
                   </div>
                   <div className='text-lg leading-8'>
                     <p>
@@ -120,11 +120,11 @@ export default function Home() {
                 {/* Second User Reveiw */}
                 <div className='flex flex-auto flex-col gap-4 lg:pr-8 xl-pr-20'>
                   <div className='flex gap-0.5 mb-2'>
-                    <Star className='h-5 w-5 text-green-600 fill-green-600 '/>
-                    <Star className='h-5 w-5 text-green-600 fill-green-600 '/>
-                    <Star className='h-5 w-5 text-green-600 fill-green-600 '/>
-                    <Star className='h-5 w-5 text-green-600 fill-green-600 '/>
-                    <Star className='h-5 w-5 text-green-600 fill-green-600 '/>
+                    <Star className='h-5 w-5 text-[#fdcc06] fill-[#fdcc06] '/>
+                    <Star className='h-5 w-5 text-[#fdcc06] fill-[#fdcc06] '/>
+                    <Star className='h-5 w-5 text-[#fdcc06] fill-[#fdcc06] '/>
+                    <Star className='h-5 w-5 text-[#fdcc06] fill-[#fdcc06]'/>
+                    <Star className='h-5 w-5 text-[#fdcc06] fill-[#fdcc06] '/>
                   </div>
                   <div className='text-lg leading-8'>
                     <p>
